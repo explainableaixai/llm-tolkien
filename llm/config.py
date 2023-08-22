1216ca7b4f206fb3c5a349cea22c5fa971dd5809
explainableaixai <58834207+explainableaixai@users.noreply.github.com>
2023-08-22 16:23:37 +0200

    Update config.py

--- llm/config.py
+++ llm/config.py
@@ -4,7 +4,7 @@ from pathlib import Path
 REPO_DIR = Path(os.path.realpath(os.path.join(os.path.dirname(__file__), '..')))
 
 # Text extraction
-url = 'https://gosafir.com/mag/wp-content/uploads/2019/12/Tolkien-J.-The-lord-of-the-rings-HarperCollins-ebooks-2010.pdf'
+url = 'https://www.alpha-quantum/a.pdf'
 header_height = 60  # Main text distance from the top of the page: to remove header
 footer_height = 540 # Remove footer
 start_page = 45
